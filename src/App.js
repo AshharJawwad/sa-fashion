464fd28b3d09045cd671f188aee9efca03d4a211
Ashharja <rockashhar10@gmail.com>
2024-07-15 16:56:07 +0530

    Added Navbar and Hero Banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Store from './Pages/Store';
+import Campaign from './Pages/Campaign';
+import Blog from './Pages/Blog';
+import Contact from './Pages/Contact';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginReg from './Pages/LoginReg';
+import Wishlist from './Pages/Wishlist';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/store' element={<Store/>}/>
+        <Route path='/campaign' element={<Campaign/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=' :productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/wishlist' element={<Wishlist/>}/>
+        <Route path='/login' element={<LoginReg/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,47 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import cart_icon from "../Assets/cart_icon.png";
+import wishlist_icon from "../Assets/wishlist_icon.png";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  const [menu,setMenu] = useState("Home");
+
   return (
+    // Navigation Bar
     <div className="navbar">
+      {/* Top Navigation Bar */}
       <div className="top-bar">
+        {/* Top Navigation Items */}
         <div className="left">
           <ul className="yourorders">
-            <li>
-                Your Orders
-            </li>
+            <li>Your Orders</li>
           </ul>
         </div>
         <div className="right">
-            <ul className="c-l-r">
-                <li>Currency</li>
-                <li>Login/Register</li>
-            </ul>
+          <ul className="c-l-r">
+            <li>Currency</li>
+            <Link style={{ textDecoration: 'none', color: "white" }} to='/login'><li>Loin/Register</li></Link>
+          </ul>
         </div>
       </div>
+      {/* Middle Navigation Bar */}
       <div className="middle-bar">
-        <p className="nav-logo">
-            <span>SA</span>Fashion
-        </p>
-        <div className="search-box">
-            <form action="">
-                <input type="text" placeholder="Search by product name" />
-
-            </form>
-        </div>
+        {/* Middle Navigation Items */}
+        <h1 className="nav-logo">
+          <span>SA</span>Fashion
+        </h1>
+        {/* Wishlist & Cart Items */}
         <div className="w-c">
-            <img src={cart_icon} alt="" />
+          <div>
+            <Link style={{ textDecoration: 'none' }} to='/wishlist'><img src={wishlist_icon} alt="" /></Link>
+            <div className="wishlist-count">0</div>
+            <Link style={{ textDecoration: 'none' }} to='/cart'><img src={cart_icon} alt="" /></Link>
+            <div className="cart-count">0</div>
+          </div>
         </div>
       </div>
+      {/* Bottom Navigation Bar */}
       <div className="bottom-bar">
+        {/* Bottom Navigation Links */}
         <ul className="nav-links">
-            <li>Home</li>
-            <li>Store</li>
-            <li>Campaign</li>
-            <li>Blog</li>
-            <li>Pages</li>
-            <li>Contact</li>
+          <li onClick={()=>{setMenu("Home")}}>
+            <Link style={{ textDecoration: 'none', color: "black" }} to='/'>Home</Link> {menu==="Home"?<hr/>:<></>}
+          </li>
+          <li onClick={()=>{setMenu("Store")}}> <Link style={{ textDecoration: 'none', color: "black" }} to='/store'>Store</Link> {menu==="Store"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Campaign")}}> <Link style={{ textDecoration: 'none', color: "black" }} to='/campaign'>Campaign</Link> {menu==="Campaign"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Blog")}}> <Link style={{ textDecoration: 'none', color: "black" }} to='/blog'>Blog</Link> {menu==="Blog"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Contact")}}> <Link style={{ textDecoration: 'none', color: "black" }} to='/contact'>Contact</Link> {menu==="Contact"?<hr/>:<></>}</li>
         </ul>
       </div>
     </div>
